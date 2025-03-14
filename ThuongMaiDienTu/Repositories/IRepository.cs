be42bc668da28bd4d68cbac0189c849e030608c6
bien123x <bienhanoi123@gmail.com>
2025-03-14 16:02:24 +0700

    sửa repository, thêm file

--- ThuongMaiDienTu/Repositories/IRepository.cs
+++ ThuongMaiDienTu/Repositories/IRepository.cs
@@ -1,23 +1,11 @@
-using System;
-using System.Linq.Expressions;
-
-namespace ThuongMaiDienTu.Repositories;
-// <T> là một kiểu dữ liệu tổng quát, có thể là Product, Order, User...
-// where T : class đảm bảo T phải là một class, không phải kiểu int, string...
-public interface IRepository<T> where T : class
+﻿namespace ThuongMaiDienTu.Repositories
 {
-    // Lấy tất cả dữ liệu
-    Task<IEnumerable<T>> GetAll();
-    // Lấy dữ liệu theo điều kiện
-    Task<IEnumerable<T>> Get(Expression<Func<T, bool>> expression);
-    // Lấy dữ liệu theo id
-    Task<T> GetById(object id);
-    // Thêm dữ liệu
-    Task Add(T entity);
-    // Xóa dữ liệu
-    Task Delete(object id);
-    // Cập nhật dữ liệu
-    Task Update(T entity);
-    // Lưu thay đổi
-    Task Save();
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
+    }
 }
--- ThuongMaiDienTu/Repositories/Repository.cs
+++ ThuongMaiDienTu/Repositories/Repository.cs
@@ -1,50 +1,50 @@
-using System;
-using System.Linq.Expressions;
-using Microsoft.EntityFrameworkCore;
-using ThuongMaiDienTu.Models;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
 using ThuongMaiDienTu.Data;
 
-namespace ThuongMaiDienTu.Repositories;
-
-public class Repository<T> : IRepository<T> where T : class
+namespace ThuongMaiDienTu.Repositories
 {
-    private readonly ApplicationDbContext _context;
-    private readonly DbSet<T> _dbSet;
-    public Repository(ApplicationDbContext context)
+    public class Repository<T> : IRepository<T> where T : class
     {
-        _context = context;
-        _dbSet = context.Set<T>();
-    }
-    public async Task<IEnumerable<T>> GetAll()
-    {
-        return await _dbSet.ToListAsync();
-    }
-    public async Task<IEnumerable<T>> Get(Expression<Func<T, bool>> expression)
-    {
-        return await _dbSet.Where(expression).ToListAsync();
-    }
-    public async Task<T> GetById(object id)
-    {
-        return await _dbSet.FindAsync(id);
-    }
-    public async Task Add(T entity)
-    {
-        await _dbSet.AddAsync(entity);
-    }
-    public async Task Delete(object id)
-    {
-        T entity = await GetById(id);
-        _dbSet.Remove(entity);
-    }
-    public async Task Update(T entity)
-    {
-        _dbSet.Attach(entity);
-        _context.Entry(entity).State = EntityState.Modified;
-    }
-    public async Task Save()
-    {
-        await _context.SaveChangesAsync();
-    }
+        private readonly DbContextApp _context;
+        private readonly DbSet<T> _dbSet;
 
+        public Repository(DbContextApp context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var entity = _dbSet.Find(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                _context.SaveChanges();
+            }
+        }
+
+        public T GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
 }
